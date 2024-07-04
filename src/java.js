0c72e650efeba60e10be7102598d7bcb6f3b9194
Muri <mcsanchezlaguna@gmail.com>
2024-07-04 18:32:19 +0200

    JS: add forecast function

--- src/java.js
+++ src/java.js
@@ -1,7 +1,6 @@
 // Search Engine
 function search(event) {
   event.preventDefault();
-
   let searchCity = document.querySelector("#form-text");
   let h2 = document.querySelector("h2");
 
@@ -47,23 +46,24 @@ function search(event) {
     filterWind.innerHTML = `${dataWind}km/h`;
     filterEmoji.innerHTML = `<img src="${response.data.condition.icon_url}"data-showcase-temperature-container-emoji" />`;
     filterNow.innerHTML = currentDateAndHour;
+
+    getForecast(dataCity);
   }
   let apikEY = "73050fa355794447f81ab5349190dotd";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchCity.value}&key=${apikEY}&units=metric`;
   axios.get(apiUrl).then(filter);
 }
 
-function displayforecast() {
+function displayforecast(response) {
+  console.log(response.data);
   let forecast = document.querySelector("#week-forecast");
 
   let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   forecastHtml = ``;
 
   days.forEach(function (days) {
-    forecastHtml =
-      forecastHtml +
-      `<div class="one-day">
-    <p class="week-forecast-date">Thu</p>
+    forecastHtml += `<div class="one-day">
+    <p class="week-forecast-date">${days}</p>
     <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-night.png" 
     class="week-forecast-emoji">
     <p class="week-forecast-temp">
@@ -76,7 +76,12 @@ function displayforecast() {
   forecast.innerHTML = forecastHtml;
 }
 
+function getForecast(city) {
+  let apiKey = "73050fa355794447f81ab5349190dotd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayforecast);
+}
+
 let searchInput = document.querySelector(".form");
 searchInput.addEventListener("submit", search);
-
-displayforecast();
+getForecast("Panama");
